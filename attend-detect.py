48b18adc7cc6eda112e5ef56f8d13d527317e6fb
Eren Copcu <95137598+ecpp@users.noreply.github.com>
2022-04-26 13:55:09 +0300

    Update attend-detect.py
    Improved user input getting. Some general improvements and changes.

--- attend-detect.py
+++ attend-detect.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import datetime
 from time import sleep
 import numpy as np
 from PIL import ImageGrab
@@ -15,32 +16,39 @@ import json
 import keyboard
 
 
-TELEGRAM_BOT_TOKEN = 'xxxx'
-TELEGRAM_CHAT_ID = 'xxxxx'
+TELEGRAM_BOT_TOKEN = 'xxx'
+TELEGRAM_CHAT_ID = 'xxx'
 lastinput = "no"
 
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 
 
-
+writeToChat = "name surnmame" #to write for attendance. in my case NAME SURNAME
 
 path = '/Users/Eren/Desktop/result.png' #path where SS of the window will be saved.
 
 len1 = 0
 len2 = 0
 len3 = 0
+timezone = 3 #in my case its +3 so its 3. it is needed for telegram api to check whether last message is sent on time!
 
 def getUserinput():
-    link = "https://api.telegram.org/botYOURTELEGRAMTOKEN" #bot will stay (botXXXXXXXXXXXX)
+    global lastinput
+    global messageTime
+    link = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/getUpdates"
     data = requests.get(link).text
     data = json.loads(data)
     Dict = data['result']
     for obj in Dict:
-        lastinput = obj['message']['text']
-    return lastinput
+        messageTime = obj['message']['date'] # UNIX time
+        messageTime = datetime.datetime.utcfromtimestamp(messageTime)
+        now = datetime.datetime.now()
+        if(((messageTime.minute == now.minute) or (messageTime.minute == now.minute-1)) and (messageTime.hour + timezone == now.hour)): 
+            lastinput = obj['message']['text']
+        
 
 def ocrProcess(): #take SS of window and process it with ocr
-    window = pygetwindow.getWindowsWithTitle('Zoom Chat')[0] #window name of zoom chat
+    window = pygetwindow.getWindowsWithTitle('Sohbet')[0]
     print("Found window.")
     x1 = window.left
     y1 = window.top
@@ -65,29 +73,28 @@ def ocrProcess(): #take SS of window and process it with ocr
     text = pytesseract.image_to_string(Image.open('/Users/Eren/Desktop/temp2.jpg'))
     end = timer()
     print("Image processing completed. It took:", (end - start))
+    print(text)
     return text
 
 while True:
     len1 = len(ocrProcess())
-    print(len(ocrProcess()))
-    sleep(2)
+    sleep(3)
     len2 = len(ocrProcess())
-    print(len(ocrProcess()))
-    sleep(2)
+    sleep(3)
     len3 = len(ocrProcess())
-    print(len(ocrProcess()))
     
     if(len2-len1 > 10 or len3-len2 > 10):
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Attendance detected...")
         bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=open(path, 'rb'))
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Do you want to attend? You have 30 seconds!")
         sleep(30)
-        if(getUserinput()=="yes"):
+        getUserinput()
+        if(lastinput=="yes" or lastinput=="Yes"):
             print("Attended!")
-            keyboard.write('What should it write down???')
+            keyboard.write(writeToChat)
             keyboard.press_and_release('enter')
         else:
             print("not attend.!")
+        lastinput = "no"
         
-    print(len(ocrProcess()))
     sleep(15)
