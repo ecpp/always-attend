b4b156d2b0469b1c7e26e0b3735768856d54ba4a
Eren Copcu <95137598+ecpp@users.noreply.github.com>
2022-08-19 14:54:06 +0200

    Update attend-detect.py

--- attend-detect.py
+++ attend-detect.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python3
-
 import datetime
 from time import sleep
-import numpy as np
-from PIL import ImageGrab
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
-from textblob import TextBlob
 import pygetwindow
 import pyautogui
 from timeit import default_timer as timer
@@ -15,46 +10,43 @@ import requests
 import json
 import keyboard
 
+# CONFIGURATION
 
 TELEGRAM_BOT_TOKEN = 'xxx'
 TELEGRAM_CHAT_ID = 'xxx'
-lastinput = "no"
+writeToChat = "name surnmame"  # to write for attendance. in my case NAME SURNAME
+path1 = '/Users/xxxx/Desktop/temp1.png'
+path2 = '/Users/xxxx/Desktop/temp2.jpg'
+timezone = 3  # in my case its +3 so its 3. it is needed for telegram api to check whether last message is sent on time!
+zoom_window_name = 'Sohbet'  # name of the zoom application window
 
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+last_input = "no"
 
 
-writeToChat = "name surnmame" #to write for attendance. in my case NAME SURNAME
-
-path = '/Users/Eren/Desktop/result.png' #path where SS of the window will be saved.
-
-len1 = 0
-len2 = 0
-len3 = 0
-timezone = 3 #in my case its +3 so its 3. it is needed for telegram api to check whether last message is sent on time!
-
-def getUserinput():
-    lastinput = "no"
-    global messageTime
+def get_user_input():
+    global last_input
     link = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/getUpdates"
     try:
         print("Telegram connection success!")
         data = requests.get(link).text
-    except:
-        print("Error while connecting to telegram servers!")
-        return lastinput
+    except Exception as e:
+        print("Error while connecting to telegram servers! ", e)
+        return last_input
     data = json.loads(data)
-    Dict = data['result']
-    for obj in Dict:
-        messageTime = obj['message']['date'] # UNIX time
-        messageTime = datetime.datetime.utcfromtimestamp(messageTime)
+    data_to_dict = data['result']
+    for obj in data_to_dict:
+        time_of_message = obj['message']['date']  # UNIX time
+        time_of_message = datetime.datetime.utcfromtimestamp(time_of_message)
         now = datetime.datetime.now()
-        if(((messageTime.minute == now.minute) or (messageTime.minute == now.minute-1)) and (messageTime.hour + timezone == now.hour)): 
-            lastinput = obj['message']['text']
-            return lastinput
-        
+        if (((time_of_message.minute == now.minute) or (time_of_message.minute == now.minute - 1)) and (
+                time_of_message.hour + timezone == now.hour)):
+            last_input = obj['message']['text']
+            return last_input
 
-def ocrProcess(): #take SS of window and process it with ocr
-    window = pygetwindow.getWindowsWithTitle('Sohbet')[0]
+
+def ocr_process():  # take SS of window and process it with ocr
+    window = pygetwindow.getWindowsWithTitle(zoom_window_name)[0]
     print("Found window.")
     x1 = window.left
     y1 = window.top
@@ -64,44 +56,54 @@ def ocrProcess(): #take SS of window and process it with ocr
     x2 = x1 + width
     y2 = y1 + height
     print("Taking screenshot.")
-    pyautogui.screenshot(path)
+    pyautogui.screenshot(path1)
     start = timer()
     print("Image processing in progress...")
-    im = Image.open(path)
-    im = im.crop((x1+20, y1+70, x2, y2-150))
-    im.save(path)
-    im = Image.open(path) # the second one 
-    im = im.filter(ImageFilter.MedianFilter()) #some corrections for ocr
+
+    # IMAGE CORRECTION AND PROCESSING
+    im = Image.open(path1)
+    im = im.crop((x1 + 20, y1 + 70, x2, y2 - 150))
+    im.save(path1)
+    im = Image.open(path1)
+    im = im.filter(ImageFilter.MedianFilter())
     enhancer = ImageEnhance.Contrast(im)
     im = enhancer.enhance(2)
     im = im.convert('1')
-    im.save('/Users/Eren/Desktop/temp2.jpg')
-    text = pytesseract.image_to_string(Image.open('/Users/Eren/Desktop/temp2.jpg'))
+
+    im.save(path2)
+    text = pytesseract.image_to_string(Image.open(path2))
     end = timer()
     print("Image processing completed. It took:", (end - start))
     print(text)
     return text
 
-while True:
-    len1 = len(ocrProcess())
+
+def attendance_detect():  # Attendance detection algorithm. Needs to be improved.
+    global last_input
+    len1 = len(ocr_process())
     sleep(3)
-    len2 = len(ocrProcess())
+    len2 = len(ocr_process())
     sleep(3)
-    len3 = len(ocrProcess())
-    
-    if(len2-len1 > 10 or len3-len2 > 10):
+    len3 = len(ocr_process())
+
+    if len2 - len1 > 10 or len3 - len2 > 10:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Attendance detected...")
-        bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=open(path, 'rb'))
+        bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=open(path1, 'rb'))
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Do you want to attend? You have 30 seconds!")
         sleep(30)
-        lastinput = getUserinput()
+        last_input = get_user_input()
         sleep(3)
-        if(lastinput=="yes" or lastinput=="Yes"):
+        if last_input == "yes" or last_input == "Yes":
             print("Attended!")
             keyboard.write(writeToChat)
             keyboard.press_and_release('enter')
         else:
             print("not attend.!")
-        lastinput = "no"
-        
+        last_input = "no"
+
     sleep(15)
+
+
+if __name__ == '__main__':
+    while True:
+        attendance_detect()
